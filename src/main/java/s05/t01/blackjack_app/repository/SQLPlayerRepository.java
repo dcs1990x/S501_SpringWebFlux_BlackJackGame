70fb8ea655c9e1aecfded5ac58811a249b1fa1ef
Daniel Caldito Serrano <dcs1990x@gmail.com>
2025-11-25 11:22:20 +0100

    refactor:

--- src/main/java/s05/t01/blackjack_app/repository/SQLPlayerRepository.java
+++ src/main/java/s05/t01/blackjack_app/repository/SQLPlayerRepository.java
@@ -1,6 +1,8 @@
 package s05.t01.blackjack_app.repository;
 
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import s05.t01.blackjack_app.model.entities.PlayerEntity;
 
 @Repository
 public interface SQLPlayerRepository extends ReactiveCrudRepository<PlayerEntity, Long> {}
