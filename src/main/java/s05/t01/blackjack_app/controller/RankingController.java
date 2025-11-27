cca35ccc713f04877a3ee8df676bbcb97ab59010
Daniel Caldito Serrano <dcs1990x@gmail.com>
2025-11-27 20:34:58 +0100

    feature: added RankingController class, dockerfile and docker_compose files

---
+++ src/main/java/s05/t01/blackjack_app/controller/RankingController.java
@@ -0,0 +1,27 @@
+package s05.t01.blackjack_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import s05.t01.blackjack_app.domain.dtos.RankingResponseDTO;
+import s05.t01.blackjack_app.service.RankingService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ranking")
+@RequiredArgsConstructor
+public class RankingController {
+
+    private final RankingService rankingService;
+
+    @GetMapping
+    public ResponseEntity<Mono<List<RankingResponseDTO>>> getPlayersRanking() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(rankingService.getPlayerRanking());
+    }
+}
